11444b558b46b576ebb4b87518be80b1314b1783
tonyryder165 <jinnha258@gmail.com>
2022-10-04 15:08:08 +0700

    Project Group 12

--- DoAn_LTW_Nhom12/WebDiDong/Controllers/LoaiSanPhamController.cs
+++ DoAn_LTW_Nhom12/WebDiDong/Controllers/LoaiSanPhamController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PagedList;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,12 +11,74 @@ namespace WebDiDong.Controllers
     public class LoaiSanPhamController : Controller
     {
         // GET: LoaiSanPham
-        public ActionResult Index(string search="")
+        public ActionResult Index(string search = "", int page = 1, int pagesize = 6)
         {
             DBDiDongEntities db = new DBDiDongEntities();
             List<LoaiSanPham> loaiSanPhams = db.LoaiSanPhams.Where<LoaiSanPham>(row => row.TenLoaiSanPham.Contains(search)).ToList();
             ViewBag.Search = search;
-            return View(loaiSanPhams);
+            return View(loaiSanPhams.ToPagedList(page, pagesize));
+        }
+
+        // GET: LoaiSanPham/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LoaiSanPham/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoaiSanPham/Create
+        [HttpPost]
+        public ActionResult Create(LoaiSanPham lsp)
+        {
+            DBDiDongEntities db = new DBDiDongEntities();
+            db.LoaiSanPhams.Add(lsp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: LoaiSanPham/Edit/5
+        public ActionResult Edit(string id)
+        {
+            DBDiDongEntities db = new DBDiDongEntities();
+            LoaiSanPham loaiSanPham = db.LoaiSanPhams.Where<LoaiSanPham>(row => row.MaLoaiSanPham == id).FirstOrDefault();
+            return View(loaiSanPham);
+        }
+
+        // POST: LoaiSanPham/Edit/5
+        [HttpPost]
+        public ActionResult Edit(LoaiSanPham lsp)
+        {
+            DBDiDongEntities db = new DBDiDongEntities();
+            LoaiSanPham loaiSanPham = db.LoaiSanPhams.Where<LoaiSanPham>(row => row.MaLoaiSanPham == lsp.MaLoaiSanPham).FirstOrDefault();
+
+            loaiSanPham.MaLoaiSanPham = lsp.MaLoaiSanPham;
+            loaiSanPham.TenLoaiSanPham = lsp.TenLoaiSanPham;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: LoaiSanPham/Delete/5
+        public ActionResult Delete(string id)
+        {
+            DBDiDongEntities db = new DBDiDongEntities();
+            LoaiSanPham loaiSanPham = db.LoaiSanPhams.Where<LoaiSanPham>(row => row.MaLoaiSanPham == id).FirstOrDefault();
+            return View(loaiSanPham);
+        }
+
+        // POST: LoaiSanPham/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string id, LoaiSanPham lsp)
+        {
+            DBDiDongEntities db = new DBDiDongEntities();
+            LoaiSanPham loaiSanPham = db.LoaiSanPhams.Where<LoaiSanPham>(row => row.MaLoaiSanPham == id).FirstOrDefault();
+            db.LoaiSanPhams.Remove(loaiSanPham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- DoAn_LTW_Nhom12/WebDiDong/Controllers/NhaSanXuatController.cs
+++ DoAn_LTW_Nhom12/WebDiDong/Controllers/NhaSanXuatController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PagedList;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,12 +11,75 @@ namespace WebDiDong.Controllers
     public class NhaSanXuatController : Controller
     {
         // GET: NhaSanXuat
-        public ActionResult Index(string search="")
+        public ActionResult Index(string search = "", int page = 1, int pagesize = 3)
         {
             DBDiDongEntities db = new DBDiDongEntities();
             List<NhaSanXuat> nhaSanXuats = db.NhaSanXuats.Where<NhaSanXuat>(row => row.TenNhaSanXuat.Contains(search)).ToList();
             ViewBag.Search = search;
-            return View(nhaSanXuats);
+            return View(nhaSanXuats.ToPagedList(page, pagesize));
+        }
+
+
+        // GET: NhaSanXuat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: NhaSanXuat/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NhaSanXuat/Create
+        [HttpPost]
+        public ActionResult Create(NhaSanXuat nsx)
+        {
+            DBDiDongEntities db = new DBDiDongEntities();
+            db.NhaSanXuats.Add(nsx);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: NhaSanXuat/Edit/5
+        public ActionResult Edit(string id)
+        {
+            DBDiDongEntities db = new DBDiDongEntities();
+            NhaSanXuat nhaSanXuat = db.NhaSanXuats.Where<NhaSanXuat>(row => row.MaNhaSanXuat == id).FirstOrDefault();
+            return View(nhaSanXuat);
+        }
+
+        // POST: NhaSanXuat/Edit/5
+        [HttpPost]
+        public ActionResult Edit(string id, NhaSanXuat nsx)
+        {
+            DBDiDongEntities db = new DBDiDongEntities();
+            NhaSanXuat nhaSanXuat = db.NhaSanXuats.Where<NhaSanXuat>(row => row.MaNhaSanXuat == nsx.MaNhaSanXuat).FirstOrDefault();
+
+            nhaSanXuat.MaNhaSanXuat = nsx.MaNhaSanXuat;
+            nhaSanXuat.TenNhaSanXuat = nsx.TenNhaSanXuat;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: NhaSanXuat/Delete/5
+        public ActionResult Delete(string id)
+        {
+            DBDiDongEntities db = new DBDiDongEntities();
+            NhaSanXuat nhaSanXuat = db.NhaSanXuats.Where<NhaSanXuat>(row => row.MaNhaSanXuat == id).FirstOrDefault();
+            return View(nhaSanXuat);
+        }
+
+        // POST: NhaSanXuat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string id, NhaSanXuat nsx)
+        {
+            DBDiDongEntities db = new DBDiDongEntities();
+            NhaSanXuat nhaSanXuat = db.NhaSanXuats.Where<NhaSanXuat>(row => row.MaNhaSanXuat == id).FirstOrDefault();
+            db.NhaSanXuats.Remove(nhaSanXuat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
