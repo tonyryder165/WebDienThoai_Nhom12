b5ecd3821d52d4600d964888ed2d549917c311e7
tonyryder165 <jinnha258@gmail.com>
2022-11-06 21:03:47 +0700

    Success Create/Edit/Delete, Fix Data0 LoaiSanPham

---
+++ DoAn_LTW_Nhom12/WebDiDong/Controllers/LoaiSanPhamController.cs
@@ -0,0 +1,26 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDiDong.Models;
+using WebDiDong.Models.BUS;
+
+namespace WebDiDong.Controllers
+{
+    public class LoaiSanPhamController : Controller
+    {
+        // GET: LoaiSanPham
+        public ActionResult Index(int id, int page = 1, int pagesize = 6)
+        {
+            var ds = LoaiSanPhamBUS.ChiTiet(id).ToPagedList(page, pagesize);
+            using (var br = new DBDiDongEntities())
+            {
+                string loai = br.Database.SqlQuery<string>("select TenLoaiSanPham from LoaiSanPham where MaLoaiSanPham = " + id + "").FirstOrDefault();
+                ViewBag.loai = loai;
+            }
+            return View(ds);
+        }
+    }
+}
---
+++ DoAn_LTW_Nhom12/WebDiDong/Models/BUS/LoaiSanPhamBUS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDiDong.Models.BUS
+{
+    public class LoaiSanPhamBUS
+    {
+        public static IEnumerable<LoaiSanPham> DanhSach()
+        {
+            var db = new DBDiDongEntities();
+            return db.LoaiSanPhams.SqlQuery("Select * from LoaiSanPham");
+        }
+
+        public static IEnumerable<SanPham> ChiTiet(int id)
+        {
+            var db = new DBDiDongEntities();
+            return db.SanPhams.SqlQuery("Select * from SanPham where MaLoaiSanPham = " + id + "");
+        }
+    }
+}
